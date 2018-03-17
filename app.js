50150c21d4f887e798983a09a6391364092a61ed
EnoneDev <Enone@protonmail.com>
2018-03-17 14:51:15 +0300

    test another knowledge base

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-var brain=require('./brain');
-var degree=2;//degree of acceptance for misspeling based on the edit distance
-var scope=3;//max of items checked for comparaison with the pseudo corpus
-var knwlg=require('./knowledge');
-brain.feed(knwlg.keyWords,knwlg.intents,scope,degree);
-var input='salut quelle est le prix de l euro';
+const brain = require('./brain');
+const degree = 2;//degree of acceptance for misspeling based on the edit distance
+const scope = 3;//max of items checked for comparaison with the pseudo corpus
+const knwlg = require('./knowledge');
+
+const knwlg2 = require('./knowledge2');
+
+brain.feed(knwlg2.keyWords, knwlg2.intents, scope, degree);
+
+const input = 'Give me the rate of the ether compared to the bitcoin';
 console.log(brain.detect(input));
-console.log(brain.extract(input, 'money'));
+console.log(brain.extract(input, 'currency'));
