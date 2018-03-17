db35788c165d166fc30fcd0430a58ec4f263a439
EnoneDev <Enone@protonmail.com>
2018-03-17 15:49:48 +0300

    correct indentation

--- index.js
+++ index.js
@@ -2,39 +2,39 @@ const brain = require('./brain');
 const _ = require('lodash');
 
 module.exports = class Brain {
-	constructor(knowledges, configs) {
-		this._brain = brain;
+  constructor(knowledges, configs) {
+    this._brain = brain;
 
-		this._knowledge = knowledges.reduce((item, result) => {
-			result.keyWords = _.merge(result.keyWords, item.keyWords);
-			result.intents = _.merge(result.intents, item.intents);
-			return result;
-		}, { keyWords: {}, intents: {} });
-		
-		this._degree = configs.degree;
-		this._scope = configs.scope;
+    this._knowledge = knowledges.reduce((item, result) => {
+      result.keyWords = _.merge(result.keyWords, item.keyWords);
+      result.intents = _.merge(result.intents, item.intents);
+      return result;
+    }, { keyWords: {}, intents: {} });
+    
+    this._degree = configs.degree;
+    this._scope = configs.scope;
 
-		this.feed();
-	}
+    this.feed();
+  }
 
-	feed() {
-		this._brain.feed(
-			this._knowledge.keyWords,
-			this._knowledge.intents,
-			this._scope,
-			this._degree
-		);
-	}
+  feed() {
+    this._brain.feed(
+      this._knowledge.keyWords,
+      this._knowledge.intents,
+      this._scope,
+      this._degree
+    );
+  }
 
-	detect(input) {
-		return this._brain.detect(input);
-	}
+  detect(input) {
+    return this._brain.detect(input);
+  }
 
-	extract(input, what) {
-		return this._brain.extract(input, what);
-	}
+  extract(input, what) {
+    return this._brain.extract(input, what);
+  }
 
-	freeExtract(input) {
-		return this._brain.extractAll(input);
-	}
+  freeExtract(input) {
+    return this._brain.extractAll(input);
+  }
 }
