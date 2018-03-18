5545c16d42b351f74b3effcdc33cf22b151d55cf
EnoneDev <Enone@protonmail.com>
2018-03-18 21:01:55 +0300

    change file name

--- example.js
+++ example.js
@@ -1,6 +1,9 @@
 const Brain = require('./index');
+
 const degree = 2;//degree of acceptance for misspeling based on the edit distance
+
 const scope = 3;//max of items checked for comparaison with the pseudo corpus
+
 const knwlg = require('./resources/knowledge');
 
 const knwlg2 = require('./resources/knowledge2');
@@ -8,5 +11,6 @@ const knwlg2 = require('./resources/knowledge2');
 const brain = new Brain([knwlg2, knwlg], { degree, scope });
 
 const input = 'Give me the rate of the ether compared to the bitcoin';
+
 console.log(brain.detect(input));
 console.log(brain.extract(input, 'currency'));
